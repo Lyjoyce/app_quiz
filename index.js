b95a168349d480d0b59c8164e6f76c1577c4d77d
lyjoice <lahely.na@gmail.com>
2024-11-17 11:10:01 +0100

    add index.js function showQuestion with innerHTML

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
+document.querySelectorAll(".difficulty-btn").forEach((btn) => {
+    btn.addEventListener("click",function(){
+        const level= btn.getAttribute("data-level")
+        loadQuestions(level)
+    })
+})
+
 let currentQuestionIndex= 0;
 let questions= [];
 let selectedDifficulty= "";
 
-function loadQuestions(choiceDifficulty = ""){
+async function loadQuestions(difficulty){
+    try {
+        const response = await fetch("questions.json");
+        const AllQuestions = await response.json();
+
+        questions=AllQuestions.filter((q)=> q.difficulty===difficulty)
+        selectedDifficulty=difficulty
+        currentQuestionIndex = 0
+
+        startQuiz()
+    }catch(error) {
+        console.log=("erreur lors du chargement des questions", error);
+    }
+}
+
+/*function loadQuestions(choiceDifficulty = ""){
     selectedDifficulty = choiceDifficulty;
     console.log("Difficulté : " + selectedDifficulty)
 
@@ -27,7 +49,7 @@ function loadQuestions(choiceDifficulty = ""){
     } catch (error) {
         console.log=("erreur lors du chargement des questions", error);
     }
-}
+}*/
 
 function startQuiz() {
     document.querySelector(".difficulty-selection").classList.add("hidden")
@@ -36,14 +58,40 @@ function startQuiz() {
 }
 
 function showQuestions() {
-    if(currentQuestionIndex.length<questions.length) {
+    if(currentQuestionIndex<questions.length) {
         console.log(questions)
         const questionData= questions[currentQuestionIndex]
         console.log( "question data" + questionData)
-    }
+        const questionContainer= document.getElementById("quiz-container")
+
+        questionContainer.innerHTML = `
+        <div class"question">
+        <p> ${questionData.question} <p/>
+        <div/>
+        <form id="quiz-form">
+         ${questionData.options
+         .map(
+                (option, index)=> `
+                <label> 
+                    <input type="radio" name="answer" value="${option}">
+                    <span class="custom-radio"></span>
+                    ${option}
+                </label>
+                `
+                )
+            .join("")}
+            <button type="button" onclick= "submitAnswer()">Soumettre</button>
+        </form>
+        `
+        }else{
+            showFinalResult()
+        }
 }
 
-function calculateScore(callback){
+
+
+
+/*function calculateScore(callback){
     const correctAnswers ={
         q1:"Paris",
         q2:"Mercure",
@@ -59,22 +107,24 @@ function calculateScore(callback){
     }
     callback(score)
 }
-/*une fonction qui a la responsabilité d'afficher le score*/
+//une fonction qui a la responsabilité d'afficher le score
 function displayResult(score, callback){
     const resultDIV = document.getElementById("result")
         //badkiks pour écrire js dans html avec ${}
     resultDIV.innerHTML= `votre score est de ${score} sur 3.`
     callback(score)
 }
-/*une fonction qui a la responsabilité d'afficher un mss en f° du score*/
+
+//une fonction qui a la responsabilité d'afficher un mss en f° du score*/
 /**
  * 
  * @param {*int} score 
  */
-function handleMessage(score){
+/*function handleMessage(score){
     const resultDIV = document.getElementById("result")
-    /*To clean the result on page*/
-    resultDIV.classList.remove("excellent","good","try-again")
+    
+    //To clean the result on page
+    /*resultDIV.classList.remove("excellent","good","try-again")
     if(score===3){
         resultDIV.innerHTML+=" <br>Excellent!"
         resultDIV.classList.add("excellent")
@@ -86,6 +136,7 @@ function handleMessage(score){
         resultDIV.classList.add("try-again")
     }
 }
+    */
 function submitQuiz() {
     calculateScore(function(score) {
         displayResult(score, function() {
@@ -126,8 +177,3 @@ document.querySelectorAll(".difficulty-btn").forEach((btn) => {
 })
 
 
-
-
-function startTimer(){
-    
-}
--- test.js
+++ test.js
@@ -74,7 +74,7 @@ document.getElementById("logout-btn").addEventListener("click", function(){
     localStorage.setItem("isAuthenticated",false)
     window.location.href="login.html"
 })
-/*
+
 document.querySelectorAll(".difficulty-btn").forEach((btn) => {
     btn.addEventListener("click",function(){
         const level= btn.getAttribute("data-level")
@@ -117,51 +117,8 @@ function showQuestions() {
         console.log( "question data" + questionData)
     }
 }
-*/
-filterSelection("all") // Execute the function and show all columns
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("column");
-  if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-  }
-}
 
-// Show filtered elements
-function w3AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
-    }
-  }
-}
 
-// Hide elements that are not selected
-function w3RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
-}
 
-// Add active class to the current button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}    
+
+
